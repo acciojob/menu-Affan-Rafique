35f2625aa45fd66c93efa17abecf9d9132eb602a
Affan-Rafique <affanrafique9@gmail.com>
2025-11-06 23:29:42 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Menu from './Menu';
-import './styles.css';
+import Menu from './Menu'; // Correct path: same directory
+import '../styles.css'; // Correct path: up one directory
 
 // Master data for all menu items
 const allMenuItems = [
